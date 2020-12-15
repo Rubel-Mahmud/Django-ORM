371aa30fbf2e1157283f620f1e73c6ad7918df53
Rubel Mahmud <rmsoft.com@gmail.com>
2020-12-15 11:34:30 -0800

    findout the user who commented and show his name beside comment

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Blog, Author, Entry, Comment
+from .models import Blog, Author, Entry, Comments
 
 
 admin.site.register(Blog)
 admin.site.register(Author)
 admin.site.register(Entry)
-admin.site.register(Comment)
+#admin.site.register(Comment)
+admin.site.register(Comments)
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, HttpResponse
 from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
-from .models import Blog, Comment
+from django.contrib.auth.models import User
+from .models import Blog, Comments
 
 
 def index(request):
@@ -30,29 +31,35 @@ def delete(request, id):
 
     return HttpResponseRedirect('/')
 
-@login_required
+#@login_required
 def item_detail(request, id):
     context = Blog.objects.all().order_by('-id')
     item = Blog.objects.get(pk=id)
-    comments = Comment.objects.all().filter(blog=item).order_by('-id')
+    comments = Comments.objects.all().filter(blog=item).order_by('-id')
+    #username = comments.author.username
+    #nameFirstCharacter = username[0]
     return render(request, 'blog/item_detail.html', {
         'item':item, 'context':context, 'comments':comments
     })
 
 @login_required
 def comments(request, id):
-    message = ''
-    context = Blog.objects.all().order_by('-id')
-    item = Blog.objects.get(pk=id)
-    if request.method == 'POST':
-        comment = request.POST.get('comment')
-        if comment != '' and comment != None:
-            addComment = Comment(blog=item, caption=comment)
-            addComment.save()
-            return HttpResponseRedirect('/blog/item/' + str(id))
-        else:
-            message = "You didn't type anything."
-
+    if request.user.is_authenticated:
+        currentUser_id = request.user.id
+        currentUser = User.objects.get(pk=currentUser_id)
+        message = ''
+        context = Blog.objects.all().order_by('-id')
+        item = Blog.objects.get(pk=id)
+        if request.method == 'POST':
+            comment = request.POST.get('comment')
+            if comment != '' and comment != None:
+                addComment = Comments(blog=item, author=currentUser, caption=comment)
+                addComment.save()
+                return HttpResponseRedirect('/blog/item/' + str(id))
+            else:
+                message = "You didn't type anything."
+    else:
+        currentUser = None
     return render(request, 'blog/comments.html', {
-        'context':context, 'item':item, 'message':message
+        'context':context, 'item':item, 'message':message, 'currentUser':currentUser
     })
